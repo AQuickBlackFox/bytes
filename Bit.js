8d8f27479940ee4de998a98ee58725c134478851
Aditya Atluri <Aditya.Atluri@amd.com>
2017-02-06 23:00:41 -0600

    added half and full adder

--- Bit.js
+++ Bit.js
@@ -1,6 +1,3 @@
-var msg = 'Hello World';
-console.log(msg);
-
 var one = 0x1;
 var zero = 0x0;
 
@@ -21,15 +18,22 @@ function Not(A) {
     if(A == one) { return zero; }
 }
 
-function Adder1Bit(A, B) {
+function HalfAdder(A, B) {
     S = Xor(A, B);
     C = And(A, B);
     return [S, C];
 }
 
+function FullAdder(A, B, C) {
+    a0 = Xor(A, B);
+    S = Xor(C, a0);
+    c0 = And(C, a0);
+    a1 = And(A, B);
+    c1 = Or(c0, a1);
+    return [S, c1];
+}
 
 var x = (Not(one)).toString(16);
 var y = (Not(zero)).toString(16);
 console.log(x);
 console.log(y);
-console.log(Adder1Bit(one,one).toString(16));
